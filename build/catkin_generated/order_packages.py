bf805345c398bf2b70b3272321b8db3f613cee89
Ahmed Elgammal <a.t.elgammal@gmail.com>
2023-11-01 21:08:15 +0200

    initial workspace

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/ahmed/projects/udacity/GoChaseIt!/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/ahmed/projects/manipulator_robot/arduinobot_ws/devel;/opt/ros/noetic'.split(';') if '/home/ahmed/projects/manipulator_robot/arduinobot_ws/devel;/opt/ros/noetic' != '' else []
