fd4273a1adf4270f5b46847805a198dff666edac
Ahmed Elgammal <a.t.elgammal@gmail.com>
2023-11-05 02:45:09 +0200

    complete the urdf design the robot chassis whith two wheels , with camera and LIDAR hokuyo and finish launch file with gazebo and rviz plugin

--- build/my_robot/CTestTestfile.cmake
+++ build/my_robot/CTestTestfile.cmake
@@ -1,8 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: /home/ahmed/projects/udacity/GoChaseIt!/src
-# Build directory: /home/ahmed/projects/udacity/GoChaseIt!/build
+# Source directory: /home/ahmed/projects/udacity/GoChaseIt!/src/my_robot
+# Build directory: /home/ahmed/projects/udacity/GoChaseIt!/build/my_robot
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("my_robot")
